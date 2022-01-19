5d2e43034f35c067e31a22d898d1c9d3f1e10bdb
Elia Dotor Puente <elia.dotor@gmail.com>
2022-01-19 17:38:24 +0100

    Créate ListViewController and ListControllerBuilder

---
+++ CursoiOS-Proyecto2/Sections/ListController/ListControllerBuilder.swift
@@ -0,0 +1,20 @@
+//
+//  ListControllerBuilder.swift
+//  CursoiOS-Proyecto2
+//
+//  Created by Elia Dotor Puente on 19/1/22.
+//
+
+import Foundation
+import UIKit
+
+class ListControllerBuilder {
+    
+    func build()-> UIViewController {
+        let viewController = ListViewController.createFromStoryBoard()
+        // inyeccion de dependencias
+        viewController.fetchLandmarks = FetchLandmarksFromDisk()
+        return viewController
+    }
+        
+}
---
+++ CursoiOS-Proyecto2/Sections/ListController/ListViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ListViewController.swift
+//  CursoiOS-Proyecto2
+//
+//  Created by Elia Dotor Puente on 19/1/22.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    // Dependencia
+    var fetchLandmarks: FetchLandmarksUseCase?
+    
+    static func createFromStoryBoard() -> ListViewController {
+        return UIStoryboard(name: "ListViewController", bundle: .main).instantiateViewController(withIdentifier: "ListViewController") as! ListViewController
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        fetchData()
+        
+    }
+    
+    private var landmarks = [Landmark]() {
+        // Para que recargue los datos cada vez que haya un cambio
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    private func fetchData(){
+        fetchLandmarks?.fetchLandmarks({result  in
+            switch result {
+            case.success(let landmarks):
+                self.landmarks = landmarks
+                // Si los datos tardan en llegar self.tableView.reloadData()
+            case.failure: break
+            }
+        })
+    }
+    
+
+}
+
+extension ListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return landmarks.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listCell", for: indexPath)
+        let landmark = landmarks[indexPath.row]
+        cell.textLabel?.text = landmark.name
+        return cell
+    }
+}
+/*
+ Es lo mismo que meter la función static directamente en la clase
+extension ListViewController {
+    static func createFromStoryBoard() -> ListViewController {
+        return UIStoryboard(name: "ListViewController", bundle: .main).instantiateViewController(withIdentifier: "ListViewController") as! ListViewController
+    }
+}*/
